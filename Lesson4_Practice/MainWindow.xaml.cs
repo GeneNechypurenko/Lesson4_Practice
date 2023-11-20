8f668fbff32acbeb3397879ac82da339fc64b368
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-11-20 20:02:41 +0200

    Add project files.

---
+++ Lesson4_Practice/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson4_Practice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            string message = $"Button {button.Content} clicked!";
+            MessageBox.Show(message, "Button Clicked", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
